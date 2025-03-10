549d706494a8c8a70364494570d0e2f1e5daad90
knoffi <matthiastutor@outlook.de>
2025-03-10 14:53:39 +0100

    #4 chore(backend): void floating promise

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
+// disable
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ["http://localhost:5173"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // Allow credentials
-    allowedHeaders: 'Content-Type,Accept,Authorization',
+    allowedHeaders: "Content-Type,Accept,Authorization",
   });
 
   await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+void bootstrap();
