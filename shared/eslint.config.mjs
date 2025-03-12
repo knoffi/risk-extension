01d3205e3672892e41bf262a482da0e185f4a475
knoffi <matthiastutor@outlook.de>
2025-03-12 14:43:49 +0100

    #4 dx(backend): add no-relative-import linting rule and remove unused code

--- shared/eslint.config.mjs
+++ shared/eslint.config.mjs
@@ -1,52 +1,61 @@
 // @ts-check
 import eslint from "@eslint/js";
+import eslintPluginForImportsInTypescript from "eslint-plugin-import-typescript";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 // TODO: use explicit rules and embrace eslint-rule-ownership (maybe even look up eslintPluginPrettierRecommended-object from node_module)
 export default tseslint.config(
-  {
-    ignores: ["eslint.config.mjs"],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+     {
+          ignores: ["eslint.config.mjs"],
+     },
+     eslint.configs.recommended,
+     ...tseslint.configs.recommendedTypeChecked,
 
-  // Adds eslintConfigPrettier and eslintPluginPrettier, but prettier-violations are warnings
-  {
-    ...eslintPluginPrettierRecommended,
-    rules: {
-      ...eslintPluginPrettierRecommended.rules,
-      "prettier/prettier": "warn",
-    },
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: "module",
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "error",
-      "@typescript-eslint/no-floating-promises": "error",
-      "@typescript-eslint/no-unsafe-argument": "warn",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/no-unsafe-assignment": "warn",
-      "@typescript-eslint/no-unsafe-call": "warn",
-      "@typescript-eslint/no-unsafe-member-access": "warn",
-      "@typescript-eslint/require-await": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/unbound-method": "warn",
-    },
-  }
+     // Adds eslintConfigPrettier and eslintPluginPrettier, but prettier-violations are warnings
+     {
+          ...eslintPluginPrettierRecommended,
+          rules: {
+               ...eslintPluginPrettierRecommended.rules,
+               "prettier/prettier": "warn",
+          },
+     },
+     {
+          languageOptions: {
+               globals: {
+                    ...globals.node,
+                    ...globals.jest,
+               },
+               ecmaVersion: 5,
+               sourceType: "module",
+               parserOptions: {
+                    projectService: true,
+                    tsconfigRootDir: import.meta.dirname,
+               },
+          },
+     },
+     {
+          plugins: {
+               "import-typescript": eslintPluginForImportsInTypescript,
+          },
+          rules: {
+               "import-typescript/no-relative-parent-imports": ["error"],
+          },
+     },
+     {
+          rules: {
+               "@typescript-eslint/no-explicit-any": "error",
+               "@typescript-eslint/no-floating-promises": "error",
+               "@typescript-eslint/no-unsafe-argument": "warn",
+               "@typescript-eslint/ban-ts-comment": "off",
+               "@typescript-eslint/no-unsafe-return": "off",
+               "@typescript-eslint/no-unsafe-assignment": "warn",
+               "@typescript-eslint/no-unsafe-call": "warn",
+               "@typescript-eslint/no-unsafe-member-access": "warn",
+               "@typescript-eslint/require-await": "warn",
+               "@typescript-eslint/no-unused-vars": "warn",
+               "@typescript-eslint/unbound-method": "warn",
+          },
+     }
 );
--- shared/socket/to-client/message.dto.ts
+++ shared/socket/to-client/message.dto.ts
@@ -5,9 +5,3 @@ export const MESSAGE_TO_CLIENT_EVENT = "response_message";
 
 export type MessageFromServer = MessageToClient;
 export const MESSAGE_FROM_SERVER_EVENT = MESSAGE_TO_CLIENT_EVENT;
-
-function forTestingLinterAndPrettier() {
-return console.log(
-          "1n19fha8z2934h2193h4ß93qsj4q54fw4pgsdfkfjhgosdfjgfpdkjgerpjgrölmtlskrtg"
-     );
-}
