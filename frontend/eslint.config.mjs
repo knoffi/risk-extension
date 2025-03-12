fc6c77e36d8676fb1d92602637f0054d281c2a72
knoffi <matthiastutor@outlook.de>
2025-03-12 11:05:32 +0100

    #4 chore(frontend): add prettier and show unformatted lines as eslint warnings

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,40 @@
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    { ignores: ["dist", "eslint.config.js"] },
+
+    // Adds eslintConfigPrettier and eslintPluginPrettier, but prettier-violations are warnings
+    {
+        ...eslintPluginPrettierRecommended,
+        rules: {
+            ...eslintPluginPrettierRecommended.rules,
+            "prettier/prettier": "warn",
+        },
+    },
+
+    {
+        extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+        },
+    }
+);
