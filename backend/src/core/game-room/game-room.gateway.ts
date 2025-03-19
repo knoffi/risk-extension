5db499c3458bda80d0aab71238f49b04361cdee9
knoffi <matthiastutor@outlook.de>
2025-03-19 14:21:20 +0100

    #5 feat(backend): add frontend preview port for cors

--- backend/src/core/game-room/game-room.gateway.ts
+++ backend/src/core/game-room/game-room.gateway.ts
@@ -16,7 +16,7 @@ import { AuthenticationService } from "src/supporting/authentication/authenticat
 
 @WebSocketGateway({
      cors: {
-          origin: "http://localhost:5173",
+          origin: ["http://localhost:4173", "http://localhost:5173"],
      },
 })
 export class GameRoomGateway {
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
      app.setGlobalPrefix("api");
 
      app.enableCors({
-          origin: ["http://localhost:5173"],
+          origin: ["http://localhost:5173", "http://localhost:4173"],
           methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 
           credentials: true, // Allow credentials
