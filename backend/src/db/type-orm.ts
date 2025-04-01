1f1aac10abe968c266a55a2e0a733c2ee96fb923
knoffi <matthiastutor@outlook.de>
2025-04-01 18:53:33 +0200

    #7 refcator(typeorm): unnecessary relative path exception

--- backend/src/db/type-orm.ts
+++ backend/src/db/type-orm.ts
@@ -1,7 +1,5 @@
-// import { defaultConfigService } from "src/supporting/config/config.service";
+import { defaultConfigService } from "src/supporting/config/config.service";
 import { DataSource } from "typeorm";
-// eslint-disable-next-line import-typescript/no-relative-parent-imports
-import { defaultConfigService } from "../supporting/config/config.service";
 
 const dataSourceOptions = defaultConfigService.getDBDataSource();
 const dataSource = new DataSource(dataSourceOptions);
