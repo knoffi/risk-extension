d6ce596f978d00f66668c34a93cd2042565ba798
knoffi <matthiastutor@outlook.de>
2025-04-09 11:28:05 +0200

    #7 feat(manage-user.page): display users

--- frontend/src/supporting/user-management/manage-user.page.tsx
+++ frontend/src/supporting/user-management/manage-user.page.tsx
@@ -1,7 +1,37 @@
+import { GetAllUserResponse } from "@shared/src/supporting/user/dto";
+import { useEffect, useState } from "react";
+import { User } from "src/supporting/authenticated/user";
+import { defaultConfigService } from "src/supporting/config/config.service";
+
 export const ManageUserPage: React.FC = () => {
+    const [users, setUsers] = useState<GetAllUserResponse | null>(null);
+
+    useEffect(() => {
+        const origin = defaultConfigService.getApiOrigin();
+        fetch(origin + "/api/user/")
+            .then((res) => res.json())
+            .then(setUsers)
+            .catch((error) => console.error(error));
+    }, []);
+
+    const panels = users ? (
+        users.map((user) => <UserPanel user={user} />)
+    ) : (
+        <p>Loading...</p>
+    );
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <p>You can manage users!</p>
+            {panels}
+        </div>
+    );
+};
+
+const UserPanel = (props: { user: User }) => {
     return (
         <div>
-            <p>You can manage users!</p>
+            <p>{props.user.name}</p>
         </div>
     );
 };
